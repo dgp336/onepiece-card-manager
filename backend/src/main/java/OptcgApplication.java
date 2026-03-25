56923c1c045263541c70fc8e4fe969c63bc75cc6
dgp336 <dgp336@inlumine.ual.es>
2026-03-25 10:36:02 +0100

    feat: tracking all new additions.

---
+++ backend/src/main/java/OptcgApplication.java
@@ -0,0 +1,13 @@
+package ual.dra.optcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptcgApplication.class, args);
+	}
+
+}
