781a26c1d7b8ac8ebbf7e9e16af3b25ffb81d6f7
dgp336 <dgp336@inlumine.ual.es>
2026-03-26 12:26:03 +0100

    fix: circular serialization between card and product, fixed using anotation @JsonIgnore in attribute card of Product.java

--- backend/optcg/src/main/java/ual/dra/optcg/entity/Product.java
+++ backend/optcg/src/main/java/ual/dra/optcg/entity/Product.java
@@ -1,5 +1,7 @@
 package ual.dra.optcg.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -11,11 +13,12 @@ import jakarta.persistence.GenerationType;
 @Entity
 @Table(name = "products")
 public class Product {
-    
+
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "card_id", nullable = false)
     private Card card;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -29,7 +32,8 @@ public class Product {
     public Product() {
     }
 
-    public Product(Card card, int productId, String url, String marketPrice, String lowPrice, String priceDate, String variant) {
+    public Product(Card card, int productId, String url, String marketPrice, String lowPrice, String priceDate,
+            String variant) {
         this.card = card;
         this.productId = productId;
         this.url = url;
@@ -42,50 +46,65 @@ public class Product {
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public Card getCard() {
         return card;
     }
+
     public void setCard(Card card) {
         this.card = card;
     }
+
     public int getProductId() {
         return productId;
     }
+
     public void setProductId(int productId) {
         this.productId = productId;
     }
+
     public String getUrl() {
         return url;
     }
+
     public void setUrl(String url) {
         this.url = url;
     }
+
     public String getMarketPrice() {
         return marketPrice;
     }
+
     public void setMarketPrice(String marketPrice) {
         this.marketPrice = marketPrice;
     }
+
     public String getLowPrice() {
         return lowPrice;
     }
+
     public void setLowPrice(String lowPrice) {
         this.lowPrice = lowPrice;
     }
+
     public String getPriceDate() {
         return priceDate;
     }
+
     public void setPriceDate(String priceDate) {
         this.priceDate = priceDate;
     }
+
     public String getVariant() {
         return variant;
     }
+
     public void setVariant(String variant) {
         this.variant = variant;
     }
-    
+
 }
